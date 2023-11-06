8d8447187f8617ed29551bea6d9035adbcaff5dd
Shahmeer09 <shahmeerrizwan921@gmail.com>
2023-11-06 16:34:33 +0500

    LOOPS-PART-3-JS

---
+++ app.js
@@ -0,0 +1,77 @@
+//question 11
+
+// let A = [24, 53, 78, 91, 12]
+
+// let largeNum = A[0]
+// let SmallNum = A[0]
+
+// for (let i = 1; i < A.length; i++){
+//     if (A[i] > largeNum){
+//         largeNum = A[i]
+//     } else if (A[i] < SmallNum){
+//         SmallNum = A[i]
+//     }
+// }
+// console.log(`The Array is ${[A]}`)
+// console.log("The largest number in the array is: " + largeNum)
+// console.log("The smallest number in the array is: " + SmallNum)
+
+//question 12
+
+// for (let i = 5; i <= 100; i += 5){
+//     document.write(`${[i]}, `)
+// }
+
+//question 13
+
+//  let students = ["Ali", "Abdullah", "Talha", "Ahsan"]
+//  let scores = [58, 73, 89, 90]
+
+//  let table = "<table border='1'><tr><th>Student Name</th><th>Score</th></tr>"
+
+//  for ( i = 0; i < students.length; i++){
+//      table += `<tr><td> ${[students[i]]} </td><td> ${[scores[i]]}</td> </tr>`
+//  }
+//  table += '</table>'
+//  document.write(table)
+
+
+//question 14 
+
+// Given array
+//  var scores = [12, 45, 3, 22, 34, 50];
+
+//  // Ask the user for the stop value
+//  var stopValue = parseInt(prompt("Enter the stop value:"));
+
+//  // Check if the input is a valid number
+//  if (!isNaN(stopValue)) {
+//      // Iterate through the array and print numbers until the stop value is reached
+//      var output = [];
+//      for (var i = 0; i < scores.length; i++) {
+//         if (scores[i] <= stopValue) {
+//             output.push(scores[i]);
+//    } else {
+//             break;
+//         }
+//    }
+    
+//    // Print the output
+//     console.log(output);
+//  } else {
+//      // Display an error message if the input is not a valid number
+//      console.log("Invalid input. Please enter a valid number.");
+//  }
+
+
+//question 15
+
+//  // Given 2D array
+//  let A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+//   // Using nested for loops to print each element on a new line
+//  for (let i = 0; i < A.length; i++) {
+//      for (let j = 0; j < A[i].length; j++) {
+//          console.log(A[i][j]);
+//      }
+//  }
